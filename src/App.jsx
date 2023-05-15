9ab39c14c327ae2b66125bf16ffb992a9bae0a2e
Urban Vadzim <Herr.Wallace@protonmail.com>
2023-05-15 14:21:46 +0300

    feat: make Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { MantineProvider } from '@mantine/core';
 import './App.css';
+import { Header } from './Components/Header/Header';
 
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <>
+      <Header />
         <div> Test! Center me!</div>
       </>
     </MantineProvider>
